ac873a2b7c243452c3c1574cd61fea484c756b08
stdenisj <stdenisjames@gmail.com>
2020-02-21 22:04:57 -0500

    created hat model

---
+++ models/hat.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Hat = new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+    color: String,
+    description: String,
+    size: {
+        type: String,
+        enum: ['Small', 'Medium', 'Large'],
+        default: 'Medium',
+    },
+    qty: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    }
+});
+
+
+module.exports = mongoose.model('Hat', Hat)
