b2511f59b11658b1f561a853b8a178894a1f6214
stdenisj <stdenisjames@gmail.com>
2020-02-21 22:10:23 -0500

    created ballon route for index

--- controllers/balloon.js
+++ controllers/balloon.js
@@ -3,3 +3,9 @@ const express = require('express');
 const Balloon = require('../models/balloon');
 
 const balloonRouter = express.Router();
+
+balloonRouter.get('/', (req, res) => {
+    Balloon.find().then( (balloons) => {
+        res.render('', { balloons });
+    });
+});
