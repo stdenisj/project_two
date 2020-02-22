c85fd0f4764ea61940909f333d2e0e940af12d47
stdenisj <stdenisjames@gmail.com>
2020-02-21 21:58:25 -0500

    created cake schema

---
+++ models/cake.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Cake = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    isVegan: Boolean,
+    img: {
+        type: String,
+        //TODO default image for cake
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    qty: {
+        type: Number,
+        required: true,
+    },
+});
+
+
+module.exports = mongoose.model('Cake', Cake);
