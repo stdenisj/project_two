18e31f5c8049a0b804e3c82543be3a0e34ad4f2f
stdenisj <stdenisjames@gmail.com>
2020-02-23 15:52:27 -0500

    commiting things

--- controllers/balloon.js
+++ controllers/balloon.js
@@ -1,16 +1,8 @@
-const express = require('express');
+let express = require('express');
+
+let balloonRouter = express.Router();
 
 const Balloon = require('../models/balloon');
-
-const balloonRouter = express.Router();
-
-balloonRouter.get('/', (req, res) => {
-    Balloon.find().then( (balloons) => {
-      res.render('balloons/balloons', { balloons });
-    }).catch( (e) => {
-        console.log(e);
-    });
-});
 
 balloonRouter.get('/new', (req, res) => {
     res.render('balloons/newBalloon');
@@ -24,17 +16,25 @@ balloonRouter.get('/:id', (req, res) => {
     });
 });
 
-balloonRouter.post('/', (req, res) => {
-    Balloon.create(req.body).then( () => {
-        res.redirect('/balloons');
-    });
-});
-
 balloonRouter.get('/:id/edit', (req, res) => {
     Balloon.findById(req.params.id).then( (balloon) => {
         res.render('balloons/editBalloon', { balloon});
     }).catch( (e) => {
         console.log(e);
+    });
+});
+
+balloonRouter.get('/', (req, res) => {
+    Balloon.find().then( (balloons) => {
+      res.render('balloons/balloons', { balloons });
+    }).catch( (e) => {
+        console.log(e);
+    });
+});
+
+balloonRouter.post('/', (req, res) => {
+    Balloon.create(req.body).then( () => {
+        res.redirect('/balloons');
     });
 });
 
