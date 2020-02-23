d2d18f7e5b7bf7d14be043a40e7f9f8f1dce6073
stdenisj <stdenisjames@gmail.com>
2020-02-23 15:17:24 -0500

    updated

--- db/connection.js
+++ db/connection.js
@@ -14,7 +14,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-  mongoose.connect('mongodb://localhost/partyStore');
+  mongoose.connect('mongodb://localhost/balloons');
 }
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
@@ -26,17 +26,10 @@ mongoose.connection.once('open', function() {
 });
 
 
-/* Step 2
- *
- * Open up a connection to the mongo database.
- *
- * NOTE: newUrlParser diables a deprecation warning
- */
-
 /* Export the mongoose object.
  *
  * This will allow us to use the same connection to our DB
  * across our different controllers.
  *
  */
-module.exports = mongoose
+module.exports = mongoose;
--- db/seeds.js
+++ db/seeds.js
@@ -7,7 +7,7 @@ let newBalloons = [
         price: 4,
         qty: 99,
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRrECexM_qlP_ZWjtW9h0xidvgiBr5TFncrgGUbekZZEOxVxZrk',
-    }
+    },
 ];
 
 
