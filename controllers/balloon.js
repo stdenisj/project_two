bdc20c9299db5eef290a6512da20ba23887092c6
stdenisj <stdenisjames@gmail.com>
2020-02-23 14:17:20 -0500

    updated routes for edit and create

--- controllers/balloon.js
+++ controllers/balloon.js
@@ -7,6 +7,8 @@ const balloonRouter = express.Router();
 balloonRouter.get('/', (req, res) => {
     Balloon.find().then( (balloons) => {
       res.render('balloons/balloons', { balloons });
+    }).catch( (e) => {
+        console.log(e);
     });
 });
 
@@ -23,6 +25,18 @@ balloonRouter.get('/:id', (req, res) => {
 balloonRouter.post('/', (req, res) => {
     Balloon.create(req.body).then( () => {
         res.redirect('/balloons');
+    });
+});
+
+balloonRouter.get('/:id/edit', (req, res) => {
+    Balloon.findById(req.params.id).then( (balloon) => {
+        res.render('balloons/editBalloon', { balloon});
+    });
+});
+
+balloonRouter.put('/:id', (req, res) => {
+    Balloon.findByIdAndUpdate(req.params.id, req.body).then( (balloon) => {
+        res.redirect('/' + balloon.id);
     });
 });
 
