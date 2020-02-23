fb6c72f25f65ad7a1a02c3ab5a945b3a95eb4379
stdenisj <stdenisjames@gmail.com>
2020-02-23 10:44:06 -0500

    updated routes

--- controllers/balloon.js
+++ controllers/balloon.js
@@ -6,25 +6,25 @@ const balloonRouter = express.Router();
 
 balloonRouter.get('/', (req, res) => {
     Balloon.find().then( (balloons) => {
-      res.render('balloons/balloons', { balloons });
+      res.render('balloons/balloons');
     });
 });
 
-// balloonRouter.get('/new', (req, res) => {
-//     // res.render('');                          Create new balloon form
-// });
+balloonRouter.get('/new', (req, res) => {
+    // res.render('');                          Create new balloon form
+});
 
-// balloonRouter.get('/:id', (req, res) => {
-//     Balloon.findById(req.params.id).then( (balloon) => {
-//         //res.render('', { balloon });           Create Show page
-//     });
-// });
+balloonRouter.get('/:id', (req, res) => {
+    Balloon.findById(req.params.id).then( (balloon) => {
+        //res.render('', { balloon });           Create Show page
+    });
+});
 
-// balloonRouter.post('/', (req, res) => {
-//     Balloon.create(req.body).then( () => {
-//         res.redirect('/balloons');
-//     });
-// });
+balloonRouter.post('/', (req, res) => {
+    Balloon.create(req.body).then( () => {
+        res.redirect('/balloons');
+    });
+});
 
 
 module.exports = balloonRouter;
